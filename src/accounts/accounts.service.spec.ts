2f83b534fb457dd6907dff04a59a5eb4e9225ea8
Romildo Júnior <jrmmendes@outlook.com>
2020-06-19 18:45:14 -0300

    Tests: Test the schema of returned object on account service when data is valid

--- src/accounts/accounts.service.spec.ts
+++ src/accounts/accounts.service.spec.ts
@@ -2,11 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as faker from 'faker/locale/pt_BR';
+import { BadRequestException } from '@nestjs/common';
 
 import { AccountsService } from './accounts.service';
 import { User } from './schemas/user.schema';
 import { RegisterUserDto } from './dto/register-user.dto';
-import {BadRequestException} from '@nestjs/common';
 
 describe('AccountsService', () => {
   let service: AccountsService;
@@ -44,17 +44,39 @@ describe('AccountsService', () => {
         password: testPassword,
         passwordConfirmation: testPassword,
       };
-      jest.spyOn(model, 'exists').mockResolvedValue(false);
-      jest.spyOn(model, 'create').mockResolvedValue({
+      
+      const passwordHash = faker.random.alphaNumeric(12);
+
+      const createdUser = {
+        _id: faker.random.alphaNumeric(),
         name: data.name,
         email: data.email,
         isActive: false,
-        passwordHash: faker.random.alphaNumeric(12),
-      } as User);
+        passwordHash,
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString(),
+      };
+
+      jest.spyOn(model, 'exists').mockResolvedValue(false);
+      jest.spyOn(model, 'create').mockResolvedValue(createdUser as any);
 
       const user = await service.registerUser(data);
 
       expect(user).toBeDefined();
+      expect(model.create).toBeCalledWith({
+        name: expect.any(String),
+        email: expect.any(String),
+        isActive: false,
+        passwordHash: expect.any(String),
+      });
+      expect(user).toMatchObject({
+        _id: expect.any(String),
+        name: expect.any(String),
+        email: expect.any(String),
+        isActive: false,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      });
     });
 
     it('When emails is already in use, expect 400 BadRequest', async () => {
