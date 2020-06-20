9dbb6438b62e424f189d0d55fc808055e9309a42
Romildo Júnior <jrmmendes@outlook.com>
2020-06-19 21:17:17 -0300

    Deps: Add mailer module, handlebars and templates dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdtapter } from '@nestjs-modules/mailer/lib/adapters/handlebars.adapter';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +17,27 @@ import { AccountsModule } from './accounts/accounts.module';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+        defaults: {
+          from: '"Vitae | Dev Resumes" <vitae-platform@gmail.com>',
+        },
+        template: {
+          dir: join(process.cwd(), 'templates'),
+          adapter: new HandlebarsAdtapter(),
+          options: {
+            strict: true,
+          },
+        }
+      }
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
